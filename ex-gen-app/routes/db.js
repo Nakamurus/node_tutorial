be4ad1b6fb055342854ad2fcd9071ce7247a07a5
nakamurusun <3872mn@gmail.com>
2019-12-28 10:07:02 +0900

    added search database page

--- ex-gen-app/routes/db.js
+++ ex-gen-app/routes/db.js
@@ -4,24 +4,50 @@ const { check, validationResult } = require('express-validator');
 const router = express.Router();
 
 const sqlite3 = require('sqlite3');
-
 const db = new sqlite3.Database('mydb.sqlite3');
 
+const knex = require('knex') ({
+    client: 'sqlite3',
+    connection: {
+        filename: 'mydb.sqlite3'
+    },
+    useNullAsDefault: true
+});
+const Bookshelf = require('bookshelf')(knex);
+
+const MyData = Bookshelf.Model.extend({
+    // as arguments of extend you write config info about model
+    tableName: 'mydata'
+});
+// Model is an object to use a table in database
+
+
 router.get('/', (req, res, next)=> {
-    // シリアライズ（直列化）とは、用意した処理を重ならずに連続実行する処理
-    db.serialize(()=> {
-        // retrieve all record
-        db.all('select * from mydata', (err, rows) => {
-            // handling when finished access to database
-            if(!err) {
-                let data = {
-                    title: 'Hello!',
-                    content: rows //retrieved record
-                 };
-                 res.render('db/index', data);
-            }
-        })
+    new MyData().fetchAll().then((collection) => {
+        let data = {
+            title: 'DB!',
+            content: collection.toArray()
+        };
+        res.render('db/index', data)
     })
+    .catch((err) => {
+        res.status(500).json({error: true, data: {message: err.message}});
+    });
+    // Below is done without BookShelf
+    // // シリアライズ（直列化）とは、用意した処理を重ならずに連続実行する処理
+    // db.serialize(()=> {
+    //     // retrieve all record
+    //     db.all('select * from mydata', (err, rows) => {
+    //         // handling when finished access to database
+    //         if(!err) {
+    //             let data = {
+    //                 title: 'Hello!',
+    //                 content: rows //retrieved record
+    //              };
+    //              res.render('db/index', data);
+    //         }
+    //     })
+    // })
 })
 
 router.get('/add', (req, res, next) => {
@@ -42,14 +68,17 @@ router.post('/add', [
     if (!errors.isEmpty()) {
         return res.status(422).json({ errors: errors.array() });
     }
-
-    let nm = req.body.name;
-    let ml = req.body.mail;
-    let ag = req.body.age;
-    db.run('insert into mydata (name, mail, age) values (?, ?, ?)', nm, ml, ag);
-    // db.runはデータベースからレコードを取り出す必要がないときに使う（→コールバックも不要）
-    // insert into TABLE (FIELD1, FIELD2, ...) values (VALUE1, VALUE2...);
-    res.redirect('/db')
+    new MyData(req.body).save().then((model) => {
+        res.redirect('/db')
+    })
+    // below is another option without BookShelf
+    // let nm = req.body.name;
+    // let ml = req.body.mail;
+    // let ag = req.body.age;
+    // db.run('insert into mydata (name, mail, age) values (?, ?, ?)', nm, ml, ag);
+    // // db.runはデータベースからレコードを取り出す必要がないときに使う（→コールバックも不要）
+    // // insert into TABLE (FIELD1, FIELD2, ...) values (VALUE1, VALUE2...);
+    // res.redirect('/db')
 })
 
 router.get('/show', (req, res, next) => {
@@ -119,6 +148,29 @@ router.post('/delete', (req, res, next) => {
     let q = "delete from mydata where id = ?";
     db.run(q, id);
     res.redirect('/db');
+})
+
+router.get('/find', (req, res, next) => {
+    let data = {
+        title: 'DB/find',
+        content: 'Input search ID',
+        form: {fstr: ''},
+        mydata: null
+    };
+    res.render('db/find', data)
+});
+
+router.post('/find', (req, res, next) => {
+    new MyData().where('id', '=', req.body.fstr).fetch()
+    .then((collection) => {
+        let data = {
+            title: 'DB!',
+            content: `search result of #id = ${req.body.fstr} `,
+            form: req.body,
+            mydata: collection
+        };
+        res.render('db/find', data)
+    })
 })
 
 module.exports = router;
